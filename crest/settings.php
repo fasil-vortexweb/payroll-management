ceff2640717dfa5cf2ebf7a38fca846f06c111fc
Muhammed Fasil K <muhammed.fasil@vortexweb.org>
2024-09-21 13:13:10 +0530

    Update settings.php

--- crest/settings.php
+++ crest/settings.php
@@ -2,7 +2,7 @@
 // define('C_REST_CLIENT_ID','local.66e7c571586536.47205453');//Application ID
 // define('C_REST_CLIENT_SECRET','L1LTkVHN5y85c87Jfp6nJoZINix60HrNYo5Yye4iS7aTups57O');//Application key
 // or
-define('C_REST_WEB_HOOK_URL', 'https://b24-g51s9r.bitrix24.com/rest/8/6596tfx7g78yi7p1/'); //url on creat Webhook
+define('C_REST_WEB_HOOK_URL', 'https://crm.springfieldproperties.ae/rest/8/l7k81vmjlu90da24/'); //url on creat Webhook
 
 define('PAYROLL_ADDITIONS_ENTITY_TYPE_ID', 1118);
 define('PAYROLL_OVERTIMES_ENTITY_TYPE_ID', 1122);
